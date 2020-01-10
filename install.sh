c40e92f142804ee5f9fa2a7efb2b96a52c6c7de6
Carlos Castaño <calbertts@gmail.com>
2020-01-10 12:30:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ platform=`uname -s`
 distro=`cat /etc/*-release 2>/dev/null | grep ^ID=[A-Za-z]* | sed s/ID=/-/g | sed s/\"//g`
 arch=`uname -m`
 
-curl -sL -o /usr/local/bin/formatcss github.com/calbertts/formatcss/releases/latest/download/formatcss-$platform$distro-$arch
-chmod a+x /usr/local/bin/formatcss
+curl -sL -o formatcss github.com/calbertts/formatcss/releases/latest/download/formatcss-$platform$distro-$arch
+chmod a+x formatcss
+sudo mv formatcss /usr/bin
 
 echo -e "\n  Great!, now you can run 'formatcss' from anywhere\n\n  Try with:\n  \e[92mformatcss --url https://www.w3schools.com/w3css/4/w3.css\n\n\e[0m"
