9be0c65925370d5382daa951fd5d38ae0660041f
Carlos Castaño <calbertts@gmail.com>
2020-01-11 19:30:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ arch=`uname -m`
 mkdir ~/.calbertts_tools
 export PATH="$PATH:~/.calbertts_tools"
 
-curl -sL -o ~/.calbertts_tools/formatcss github.com/calbertts/formatcss/releases/latest/download/formatcss-$platform$distro-$arch
-chmod a+x ~/.calbertts_tools/formatcss
+curl -sL -o formatcss github.com/calbertts/formatcss/releases/latest/download/formatcss-$platform$distro-$arch
+chmod a+x formatcss
+mv formatcss ~/.calbertts_tools
 
 printf "\n  Great!, now you can run 'formatcss' from anywhere\n\n  Try with:\n  \e[92mformatcss --url https://www.w3schools.com/w3css/4/w3.css\n\n\e[0m"
