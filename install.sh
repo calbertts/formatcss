62ca98a7972895a1f3339e583c4ea1b978361633
Carlos Castaño <calbertts@gmail.com>
2020-01-11 19:57:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ install() {
     mkdir -p ~/.calbertts_tools
     PATH="$PATH:$HOME/.calbertts_tools"
     export PATH="$PATH:$HOME/.calbertts_tools"
-    echo "PATH=\"$PATH:$HOME/.calbertts_tools\"" >> ~/.bash_profile
-    echo "PATH=\"$PATH:$HOME/.calbertts_tools\"" >> /etc/profile
+    echo "export PATH=\"$PATH:$HOME/.calbertts_tools\"" >> ~/.bash_profile
+    echo "export PATH=\"$PATH:$HOME/.calbertts_tools\"" >> /etc/profile
 
     curl -sL -o formatcss github.com/calbertts/formatcss/releases/latest/download/formatcss-$platform$distro-$arch
     chmod a+x formatcss
